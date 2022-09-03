af099c52bfc08fca445294f500d5bbb779dd0065
constadry <behappydtworry@gmail.com>
2022-09-03 17:14:18 +0300

    feat: added mapping articles to the web page

---
+++ RssFider/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RssFider.Services;
+
+namespace RssFider.Controllers;
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        var articles = HabrBot.GetArticles();
+        return View(articles);
+    }
+}
