8da852af677c26f137c27b289947d8f5899af1be
constadry <behappydtworry@gmail.com>
2022-09-03 17:11:10 +0300

    feat: added model

---
+++ RssFider/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace RssFider.Models;
+
+public class Article
+{
+    public string Title { get; set; }
+    public string ReleaseDate { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+}
